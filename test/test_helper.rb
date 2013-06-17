3b4c0ebe5474b4e2814825c70161bc7c1b213a02
Dylan Griffin <griffindy@gmail.com>
2013-06-17 12:51:01 -0400

    Using mocha for stubs.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,9 @@
 $LOAD_PATH << File.expand_path("../../lib", __FILE__)
 
 require 'test/unit'
+require 'mocha/setup'
 require 'cookie_monster'
 
 CookieMonster.configure do |config|
   config.key = 'a key'
-  config.iv = 'an iv'
 end
