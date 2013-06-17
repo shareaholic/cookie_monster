d84e66ed0bf7c6a2108fb723fd1a9b7b1f3f8f81
Dylan Griffin <griffindy@gmail.com>
2013-06-17 12:52:26 -0400

    Configuration#iv now returns a different value each call.

--- lib/cookie_monster/configuration.rb
+++ lib/cookie_monster/configuration.rb
@@ -1,9 +1,13 @@
 module CookieMonster
   class Configuration
-    attr_accessor :key, :iv, :cipher_type
+    attr_accessor :key, :cipher_type
 
     def initialize
       @cipher_type = 'AES-256-CBC'
+    end
+
+    def iv
+      rand.to_s
     end
   end
 end
--- test/test_cookie_monster.rb
+++ test/test_cookie_monster.rb
@@ -4,14 +4,16 @@ class CookieMonsterTest < Test::Unit::TestCase
   def test_configuration
     CookieMonster.configure do |config|
       config.key = 'a key'
-      config.iv = 'iv'
     end
 
     assert_equal 'a key', CookieMonster.configuration.key
-    assert_equal 'iv', CookieMonster.configuration.iv
   end
 
   def test_default_cipher_type
     assert_equal 'AES-256-CBC', CookieMonster.configuration.cipher_type
+  end
+
+  def test_iv_not_the_same
+    assert_not_equal CookieMonster.configuration.iv, CookieMonster.configuration.iv
   end
 end
